990f2f3c0607ae750604c48517dbbd4e8a4c82bb
teegash <nategadgets@gmail.com>
2024-01-30 14:33:10 +0300

    added add_staff page and save

--- student_management_app/HodViews.py
+++ student_management_app/HodViews.py
@@ -1,5 +1,36 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib import messages
+
+from student_management_app.models import CustomUser
 
 
 def admin_home(request):
     return render(request, "hod_template/home_content.html")
+
+def add_staff(request):
+    return render(request, "hod_template/add_staff_template.html")
+
+def add_staff_save(request):
+    if request.method!="POST":
+        return HttpResponse("Method Not Allowed")
+    else:
+        first_name=request.POST.get("first_name")
+        last_name=request.POST.get("last_name")
+        username=request.POST.get("username")
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        address=request.POST.get("address")
+        try:
+            user=CustomUser.objects.create_user(username=username,last_name=last_name,first_name=first_name,password=password,email=email,user_type=2)
+            user.staffs.address=address
+            user.save()
+            messages.success(request,"Successfully Added Staff")
+            return HttpResponseRedirect("/add_staff")
+        except:
+            messages.success(request,"Failed To Add Staff")
+            return HttpResponseRedirect("/add_staff")
+            
+             
+             
+             
--- student_management_system/urls.py
+++ student_management_system/urls.py
@@ -29,5 +29,7 @@ urlpatterns = [
     path('get_user_details', views.GetUserDetails),
     path('logout_user', views.logout_user),
     path('doLogin',views.doLogin),
-    path('admin_home',HodViews.admin_home)
+    path('admin_home',HodViews.admin_home),
+    path('add_staff', HodViews.add_staff),
+    path('add_staff_save', HodViews.add_staff_save),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
