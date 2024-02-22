3e0202c972008b57b8cabbe235e994eb1d29d821
teegash <nategadgets@gmail.com>
2024-02-22 12:35:04 +0300

    used pure django standard for class coz its more securem

--- student_management_app/HodViews.py
+++ student_management_app/HodViews.py
@@ -58,47 +58,52 @@ def add_course_save(request):
         
         
 def add_student(request):
-    courses=Courses.objects.all()
     form=AddStudentForm()
-    return render(request, "hod_template/add_student_template.html", {"courses":courses, "form":form}) 
+    return render(request, "hod_template/add_student_template.html", {"form":form}) 
 
 
 def add_student_save(request):
     if request.method!="POST":
         return HttpResponse("Method Not Allowed")
     else:
-        first_name=request.POST.get("first_name")
-        last_name=request.POST.get("last_name")
-        username=request.POST.get("username")
-        email=request.POST.get("email")
-        password=request.POST.get("password")
-        address=request.POST.get("address")
-        session_start=request.POST.get("session_start")
-        session_end=request.POST.get("session_end")
-        course_id=request.POST.get("course")
-        sex=request.POST.get("sex")
-        
-        profile_pic=request.FILES['profile_pic']
-        fs=FileSystemStorage()
-        filename=fs.save(profile_pic.name,profile_pic)
-        profile_pic_url=fs.url(filename)
-        
-        
-        try:
-            user=CustomUser.objects.create_user(username=username,last_name=last_name,first_name=first_name,password=password,email=email,user_type=3)
-            user.students.address=address
-            course_obj=Courses.objects.get(id=course_id)
-            user.students.course_id=course_obj
-            user.students.session_start_year=session_start
-            user.students.session_end_year=session_end
-            user.students.gender=sex
-            user.students.profile_pic=profile_pic_url
-            user.save()
-            messages.success(request,"Successfully Added Student")
-            return HttpResponseRedirect("/add_student")
-        except:
-            messages.error(request,"Failed To Add Student")
-            return HttpResponseRedirect("/add_student")
+        form=AddStudentForm(request.POST,request.FILES)
+        if form.is_valid():
+            first_name=form.cleaned_data["first_name"]
+            last_name=form.cleaned_data["last_name"]
+            username=form.cleaned_data["username"]
+            email=form.cleaned_data["email"]
+            password=form.cleaned_data["password"]
+            address=form.cleaned_data["address"]
+            session_start=form.cleaned_data["session_start"]
+            session_end=form.cleaned_data["session_end"]
+            course_id=form.cleaned_data["course"]
+            sex=form.cleaned_data["sex"]
+            
+            profile_pic=request.FILES['profile_pic']
+            fs=FileSystemStorage()
+            filename=fs.save(profile_pic.name,profile_pic)
+            profile_pic_url=fs.url(filename)
+            
+            
+            try:
+                user=CustomUser.objects.create_user(username=username,last_name=last_name,first_name=first_name,password=password,email=email,user_type=3)
+                user.students.address=address
+                course_obj=Courses.objects.get(id=course_id)
+                user.students.course_id=course_obj
+                user.students.session_start_year=session_start
+                user.students.session_end_year=session_end
+                user.students.gender=sex
+                user.students.profile_pic=profile_pic_url
+                user.save()
+                messages.success(request,"Successfully Added Student")
+                return HttpResponseRedirect("/add_student")
+            except:
+                messages.error(request,"Failed To Add Student")
+                return HttpResponseRedirect("/add_student")
+        else:
+            form=AddStudentForm(request.POST)
+            return render(request, "hod_template/add_student_template.html", {"form":form})
+            
     
 
 def add_subject(request):
--- student_management_app/forms.py
+++ student_management_app/forms.py
@@ -6,12 +6,12 @@ class DateInput(forms.DateInput):
     input_type = "date"
 
 class AddStudentForm(forms.Form):
-    email=forms.EmailField(label="Email",max_length=50,widget=forms.EmailInput())
-    password=forms.CharField(label="Password",max_length=50,widget=forms.PasswordInput())
-    first_name=forms.CharField(label="First Name",max_length=50)
-    last_name=forms.CharField(label="Last Name",max_length=50)
-    username=forms.CharField(label="Username",max_length=50)
-    address=forms.CharField(label="Address",max_length=50)
+    email=forms.EmailField(label="Email",max_length=50,widget=forms.EmailInput(attrs={"class":"form-control"}))
+    password=forms.CharField(label="Password",max_length=50,widget=forms.PasswordInput(attrs={"class":"form-control"}))
+    first_name=forms.CharField(label="First Name",max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
+    last_name=forms.CharField(label="Last Name",max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
+    username=forms.CharField(label="Username",max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
+    address=forms.CharField(label="Address",max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
     
     courses=Courses.objects.all()
     course_list=[]
@@ -24,8 +24,8 @@ class AddStudentForm(forms.Form):
         ("Female","Female")
     )
         
-    course=forms.ChoiceField(label="Course",choices=course_list)
-    sex=forms.ChoiceField(label="Sex",choices=gender_choice)
-    session_start=forms.DateField(label="Session Start",widget=DateInput())
-    session_end=forms.DateField(label="Session End",widget=DateInput())
-    profile_pic=forms.FileField(label="Profile Pic",max_length=50)
+    course=forms.ChoiceField(label="Course",choices=course_list,widget=forms.Select(attrs={"class":"form-control"}))
+    sex=forms.ChoiceField(label="Sex",choices=gender_choice,widget=forms.Select(attrs={"class":"form-control"}))
+    session_start=forms.DateField(label="Session Start",widget=DateInput(attrs={"class":"form-control"}))
+    session_end=forms.DateField(label="Session End",widget=DateInput(attrs={"class":"form-control"}))
+    profile_pic=forms.FileField(label="Profile Pic",max_length=50,widget=forms.FileInput(attrs={"class":"form-control"}))
