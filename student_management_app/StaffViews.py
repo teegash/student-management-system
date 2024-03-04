c6a08af3df3efc09ff9e4d2e103e38a8cfbcd81f
teegash <nategadgets@gmail.com>
2024-03-04 10:03:46 +0300

    take attendance save student

--- student_management_app/StaffViews.py
+++ student_management_app/StaffViews.py
@@ -31,5 +31,27 @@ def get_students(request):
         
     return JsonResponse(json.dumps(list_data),content_type="application/json",safe=False)
     
-
+@csrf_exempt
+def save_attendance_data(request):
+    student_ids=request.POST.getlist("student_ids[]")
+    print(student_ids)
+    return HttpResponse("OK")
+    # subject_id=request.POST.get("subject_id")
+    # attendance_date=request.POST.get("attendance_date")
+    # session_year_id=request.POST.get("session_year_id")
+    
+    # subject_model=Subjects.objects.get(id=subject_id)
+    # session_model=SessionYearModel.object.get(id=session_year_id)
+    # json_student=json.loads(student_ids)
+    # print(subject_model)
+    # print(session_model)
+    # print(attendance_date)
+    # print(json_student)
+    
+    # for student_id in json_student:
+    #     student=Students.objects.get(admin=student_id)
+    #     attendance=StudentsAttendance(student_id=student,subject_id=subject_model,attendance_date=attendance_date,session_year_id=session_model)
+    #     attendance.save()
+    
+    # return HttpResponse("OK")
 
--- student_management_system/urls.py
+++ student_management_system/urls.py
@@ -56,6 +56,7 @@ urlpatterns = [
     path('staff_home', StaffViews.staff_home,name="staff_home"),
     path('staff_take_attendance', StaffViews.staff_take_attendance,name="staff_take_attendance"),
     path('get_students', StaffViews.get_students,name="get_students"),
+    path('save_attendance_data', StaffViews.save_attendance_data,name="save_attendance_data"),
     # Student URL Path
     path('student_home', StudentViews.student_home,name="student_home"),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
