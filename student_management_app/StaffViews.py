1c036e8cd7ba302eff0a7bf6a837b000508e7ca4
teegash <nategadgets@gmail.com>
2024-03-08 09:03:12 +0300

    fetch attendance button

--- student_management_app/StaffViews.py
+++ student_management_app/StaffViews.py
@@ -58,8 +58,20 @@ def save_attendance_data(request):
     
 def staff_update_attendance(request):
     subjects=Subjects.objects.filter(staff_id=request.user.id)
-    attendance=Attendance.objects.all()
-    return render(request,"staff_template/staff_update_attendance.html",{"subjects":subjects,"session_years":session_years})
+    session_year_id=SessionYearModel.object.all()
+    return render(request,"staff_template/staff_update_attendance.html",{"subjects":subjects,"session_year_id":session_year_id})
     
+@csrf_exempt
+def get_attendance_dates(request):
+    subject=request.POST.get("subject")
+    session_year_id=request.POST.get("session_year_id")
+    subject_obj=Subjects.objects.get(id=subject)
+    session_year_obj=SessionYearModel.object.get(id=session_year_id)
+    attendance=Attendance.objects.filter(subject_id=subject_obj,session_year_id=session_year_obj)
+    attendance_obj=[]
+    for attendance_single in attendance:
+        data={"id":attendance_single.id,"attendance_date":str(attendance_single.attendance_date),"session_year_id":attendance_single.session_year_id.id}
+        attendance_obj.append(data)
     
+    return JsonResponse(json.dumps(attendance_obj),safe=False)
 
